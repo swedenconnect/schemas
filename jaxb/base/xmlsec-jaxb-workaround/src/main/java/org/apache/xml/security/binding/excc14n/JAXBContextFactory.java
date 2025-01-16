6a558e4a33febc356275d47f0130a0a64fd3d567
Martin Lindström <martin.lindstrom@litsec.se>
2025-01-16 11:09:44 +0100

    IS-42 Updated copyright statement

--- jaxb/base/xmlsec-jaxb-workaround/src/main/java/org/apache/xml/security/binding/excc14n/JAXBContextFactory.java
+++ jaxb/base/xmlsec-jaxb-workaround/src/main/java/org/apache/xml/security/binding/excc14n/JAXBContextFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020-2023 Sweden Connect AB
+ * Copyright 2017-2025 Sweden Connect
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
